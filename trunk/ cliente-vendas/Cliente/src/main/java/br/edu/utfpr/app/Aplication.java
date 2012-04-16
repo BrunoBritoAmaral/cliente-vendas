f65c4de4cf881f2eefe9cac3a9873a1c01c7b6ee
luanmalaguti@gmail.com <luanmalaguti@gmail.com@143b8971-3ad4-e2f1-0c14-650b026c8c98>
2012-04-16 23:17:21 +0000

    tela principal

--- trunk/ cliente-vendas/Cliente/src/main/java/br/edu/utfpr/app/Aplication.java
+++ trunk/ cliente-vendas/Cliente/src/main/java/br/edu/utfpr/app/Aplication.java
@@ -1,7 +1,32 @@
 package br.edu.utfpr.app;
 
+import br.edu.utfpr.view.principal.ClienteView;
+
 public class Aplication {
-	public static void main(String[] args) {
-		System.out.println("Oi, meu nome Silvio Maoeeee");
-	}
+	
+    public static void main(String args[]) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(ClienteView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(ClienteView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(ClienteView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(ClienteView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+     
+        java.awt.EventQueue.invokeLater(new Runnable() {
+
+            public void run() {
+                new ClienteView().setVisible(true);
+            }
+        });
+    }
 }
