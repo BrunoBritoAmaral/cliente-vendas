47966d7719deb019af56a6d5341ec19b99716a50
luanmalaguti@gmail.com <luanmalaguti@gmail.com@143b8971-3ad4-e2f1-0c14-650b026c8c98>
2012-04-17 02:58:39 +0000

    Pojos sem as anotações no cliente, sómente para serialização. Criação do cadastro genérico

---
+++ trunk/ cliente-vendas/Cliente/src/main/java/br/edu/utfpr/app/beans/Pedido.java
@@ -0,0 +1,55 @@
+package br.edu.utfpr.app.beans;
+
+import br.edu.utfpr.util.IBean;
+
+
+public class Pedido implements IBean{
+
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	
+	private String descricao;
+
+	private Cliente cliente;
+	
+
+	private Vendedor vendedor;
+	
+	public Pedido() {
+	}
+
+	@Override
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setVendedor(Vendedor vendedor) {
+		this.vendedor = vendedor;
+	}
+
+	public Vendedor getVendedor() {
+		return vendedor;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
+}
