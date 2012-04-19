f77cab12893477c3de22c7a69b4fbe9d00a3e95c
gerrardpardie <gerrardpardie@gmail.com>
2012-04-19 01:04:00 +0000

    O Cliente agora envia e recebe respostas

---  cliente-vendas/Cliente/src/main/java/br/edu/utfpr/util/Client.java
+++  cliente-vendas/Cliente/src/main/java/br/edu/utfpr/util/Client.java
@@ -1,6 +1,7 @@
 package br.edu.utfpr.util;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -13,6 +14,7 @@ import br.edu.utfpr.app.dto.ClienteDTO;
 public class Client{
 	static final int port = 56550;
 	static ObjectOutputStream oos;
+	static ObjectInputStream ois;
 	static Socket client;
 	
 	public Client(){
@@ -23,7 +25,11 @@ public class Client{
     	try{
     		oos.writeUTF(new String(mensagem));
     		oos.writeObject(o);
+    		//Agora aqui tem que receber um objeto e manipula-lo
+    		String texto = ois.readUTF();
+    		System.out.println("Texto:"+texto);
     		oos.flush();
+    		
         }catch( Exception e ){ }			
     }
     
@@ -31,6 +37,7 @@ public class Client{
     	try {
 			client = new Socket ( "localhost",56551);
 			oos = new ObjectOutputStream( client.getOutputStream() );
+			ois = new ObjectInputStream(client.getInputStream());
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
